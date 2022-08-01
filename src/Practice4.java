73349043412f56994095a534caae8ef9cdc909b9
GitDemo <wjsalswo985@naver.com>
2022-08-01 17:50:54 +0900

    real last

--- src/Practice4.java
+++ src/Practice4.java
@@ -4,25 +4,20 @@ public class Practice4 {
 		int [] arr = {10, 50, 44, 56, 29, 30, 9, 48, 92 ,84};
 		int sum = 0;
 		double avg = 0.0;
-		int min = 0;
-		int max = 0;
+		int min;
+		int max;
 		int len = arr.length;
 		System.out.print("배열 원본 = ");
 		for(int i = 0; i<arr.length;i++) {
 			System.out.print(arr[i]);
-			if(i!=arr.length-1)
-				System.out.print(", ");
+			if(i!=arr.length-1)System.out.print(", ");
 			sum += arr[i];
 		}
 		min = arr[0];
 		max = arr[len-1];
 		for(int i = 0 ; i<arr.length;i++) {
-			if(min>arr[i]) {
-				min=arr[i];
-			}
-			if(max<arr[i]) {
-				max = arr[i];
-			}
+			if(min>arr[i])min=arr[i];
+			if(max<arr[i])max = arr[i];
 		}
 		avg =(double)sum/len;
 		System.out.println();
@@ -36,8 +31,7 @@ public class Practice4 {
 				}
 			}
 			System.out.print(arr[i]);
-			if(i!=arr.length-1)
-				System.out.print(", ");
+			if(i!=arr.length-1)System.out.print(", ");
 		}
 		
 		System.out.println();
@@ -51,10 +45,8 @@ public class Practice4 {
 				}
 			}
 			System.out.print(arr[i]);
-			if(i!=arr.length-1)
-				System.out.print(", ");
+			if(i!=arr.length-1)System.out.print(", ");
 		}
-		
 		System.out.println();
 		System.out.println("합계 = "+sum);
 		System.out.println("평균 = "+avg);
