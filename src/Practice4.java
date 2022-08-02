ab50f3cde631a2c9cb1cd993e54f7f887cb4af12
GitDemo <wjsalswo985@naver.com>
2022-08-02 09:05:53 +0900

    마지막

--- src/Practice4.java
+++ src/Practice4.java
@@ -8,22 +8,22 @@ public class Practice4 {
 		int max;
 		int len = arr.length;
 		System.out.print("배열 원본 = ");
-		for(int i = 0; i<arr.length;i++) {
+		for(int i = 0; i<len;i++) {
 			System.out.print(arr[i]);
-			if(i!=arr.length-1)System.out.print(", ");
+			if(i!=len-1)System.out.print(", ");
 			sum += arr[i];
 		}
 		min = arr[0];
 		max = arr[len-1];
-		for(int i = 0 ; i<arr.length;i++) {
+		for(int i = 0 ; i<len;i++) {
 			if(min>arr[i])min=arr[i];
 			if(max<arr[i])max = arr[i];
 		}
 		avg =(double)sum/len;
 		System.out.println();
 		System.out.print("내림차순 = ");
-		for(int i = 0; i<arr.length;i++) {
-			for(int j = i+1; j<arr.length;j++) {
+		for(int i = 0; i<len;i++) {
+			for(int j = i+1; j<len;j++) {
 				if(arr[i]<arr[j]) {
 					int temp = arr[j];
 					arr[j]=arr[i];
@@ -31,13 +31,13 @@ public class Practice4 {
 				}
 			}
 			System.out.print(arr[i]);
-			if(i!=arr.length-1)System.out.print(", ");
+			if(i!=len)System.out.print(", ");
 		}
 		
 		System.out.println();
 		System.out.print("올림차순 = ");
-		for(int i = 0; i<arr.length;i++) {
-			for(int j = i+1; j<arr.length;j++) {
+		for(int i = 0; i<len;i++) {
+			for(int j = i+1; j<len;j++) {
 				if(arr[i]>arr[j]) {
 					int temp = arr[j];
 					arr[j]=arr[i];
@@ -45,7 +45,7 @@ public class Practice4 {
 				}
 			}
 			System.out.print(arr[i]);
-			if(i!=arr.length-1)System.out.print(", ");
+			if(i!=len)System.out.print(", ");
 		}
 		System.out.println();
 		System.out.println("합계 = "+sum);
